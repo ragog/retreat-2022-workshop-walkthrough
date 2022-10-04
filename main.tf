9e41def7a1448567b9e512a7d24c1d8976fca57f
ragog <giovanni@checklyhq.com>
2022-10-05 01:42:45 +0200

    refactor: inline script

--- main.tf
+++ main.tf
@@ -71,7 +71,36 @@ resource "checkly_check" "browser-check" {
     "eu-central-1"
   ]
 
-  script = file("${path.module}/scripts/dadjoke.js")
+  script = <<EOT
+const { chromium } = require('playwright')
+const expect = require('expect');
+
+(async () => {
+	const browser = await chromium.launch();
+	const context = await browser.newContext();
+
+	const page = await context.newPage();
+
+	await page.goto('https://icanhazdadjoke.com/');
+
+	await page.locator('#nav-menu a:has-text("Search jokes")').click();
+
+	await page.locator('[placeholder="Search terms\\.\\.\\."]').click();
+	await page.locator('[placeholder="Search terms\\.\\.\\."]').fill('orange');
+	await page.locator('[placeholder="Search terms\\.\\.\\."]').press('Enter');
+
+	await page.screenshot({ path: 'scr1.png' })
+
+	const rows = page.locator('table tr');
+	const count = await rows.count()
+
+	expect(count).toEqual(8)
+
+	await context.close();
+	await browser.close();
+})();
+
+EOT
 
   alert_channel_subscription {
     channel_id = checkly_alert_channel.email_channel.id
